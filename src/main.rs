d9d0df476c2fb3823f876abdf9408b548f28a21a
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-06-07 10:37:43 +0000

    bump pg threshold

--- src/main.rs
+++ src/main.rs
@@ -130,7 +130,7 @@ async fn caching_performance(opts: &Opts) {
         sqlite_none: 150,
         postgresql_none: 100,
         sqlite_truncate_all: 60,
-        postgresql_truncate_all: 40,
+        postgresql_truncate_all: 45,
         sqlite_truncate: 50,
         postgresql_truncate: 40,
         sqlite_trigger: 10,
