77c31213f0c85c6cdb7a0b429043b0606f4c1b65
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-06-25 10:20:57 -0400

    change test

--- src/rltbl_driver.rs
+++ src/rltbl_driver.rs
@@ -100,18 +100,16 @@ impl BenchSuite for RltblDriver {
         self.pool
             .query(
                 &format!(
-                    "SELECT foo, COUNT(bar) \
+                    "SELECT foo, bar \
                      FROM rltbl_driver_view \
                      WHERE foo > {pp}1
-                     GROUP BY foo \
-                     HAVING COUNT(bar) > {pp}2 \
                      ORDER BY foo",
                     pp = match self.pool.kind() {
                         DbKind::SQLite => "?",
                         DbKind::PostgreSQL => "$",
                     }
                 ),
-                &params![0_i32, 20_i64],
+                &params![0_i32],
             )
             .await
             .unwrap();
